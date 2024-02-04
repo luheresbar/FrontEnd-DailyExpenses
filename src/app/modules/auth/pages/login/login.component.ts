b1adad08f85065da4990ce53ce6e87da0443ee51
luheresbar <luheresbar@gmail.com>
2024-02-04 11:35:02 -0500

    Configuracion del componente background en auth

---
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { BackgroundComponent } from '../../components/background/background.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { LoginFormComponent } from '../../components/login-form/login-form.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, BackgroundComponent, HeaderComponent, LoginFormComponent, FooterComponent],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
