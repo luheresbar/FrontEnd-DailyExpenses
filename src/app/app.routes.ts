bcf9a244a943c8721f2d67fdf4d716e298ab68a7
luheresbar <luheresbar@gmail.com>
2024-02-05 16:11:03 -0500

    Creacion de los componentes: /auth/pages/register y /auth/component/social-media

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './modules/auth/pages/login/login.component';
+import { RegisterComponent } from './modules/auth/pages/register/register.component';
 
 export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   }
 ];
