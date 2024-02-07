9c3a9043bb3b7a331dd711e45a9c36c2a4b8aff6
luheresbar <luheresbar@gmail.com>
2024-02-07 13:03:32 -0500

    Maquetacion de Navbar mobile

--- src/app/modules/transactions/pages/transaction-summary/transaction-summary.component.ts
+++ src/app/modules/transactions/pages/transaction-summary/transaction-summary.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { AuthContainerComponent } from '../../../auth/components/auth-container/auth-container.component';
 import { TransactionDetailComponent } from '../../components/transaction-detail/transaction-detail.component';
+import { NavbarComponent } from '../../../layout/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-transaction-summary',
   standalone: true,
-  imports: [CommonModule, AuthContainerComponent, TransactionDetailComponent],
+  imports: [CommonModule, AuthContainerComponent, NavbarComponent],
   templateUrl: './transaction-summary.component.html',
   styleUrl: './transaction-summary.component.scss'
 })
