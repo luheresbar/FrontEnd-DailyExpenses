0827b587217c030307f6deae021d788c09bd4b76
luheresbar <luheresbar@gmail.com>
2024-02-07 14:15:14 -0500

    maquetacion de componente transaction-filter

---
+++ src/app/modules/layout/components/transaction-filter/transaction-filter.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-filter',
+  standalone: true,
+  imports: [],
+  templateUrl: './transaction-filter.component.html',
+  styleUrl: './transaction-filter.component.scss',
+})
+export class TransactionFilterComponent {
+  
+  activeButton: string = '';
+
+  constructor() {}
+
+  setActiveButton(button: string) {
+    this.activeButton = button;
+  }
+}
--- src/app/modules/transactions/pages/transaction-summary/transaction-summary.component.ts
+++ src/app/modules/transactions/pages/transaction-summary/transaction-summary.component.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { AuthContainerComponent } from '../../../auth/components/auth-container/auth-container.component';
-import { TransactionDetailComponent } from '../../components/transaction-detail/transaction-detail.component';
 import { NavbarComponent } from '../../../layout/components/navbar/navbar.component';
+import { TransactionFilterComponent } from '../../../layout/components/transaction-filter/transaction-filter.component';
 
 @Component({
   selector: 'app-transaction-summary',
   standalone: true,
-  imports: [CommonModule, AuthContainerComponent, NavbarComponent],
+  imports: [CommonModule, AuthContainerComponent, NavbarComponent, TransactionFilterComponent],
   templateUrl: './transaction-summary.component.html',
   styleUrl: './transaction-summary.component.scss'
 })
