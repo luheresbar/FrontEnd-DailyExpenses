ffb8eb83adc9895dabd1f718c56aadbab59f5773
luheresbar <luheresbar@gmail.com>
2024-02-20 18:02:15 -0500

    Creacion de los componentes category form and category-summary

---
+++ src/app/modules/categories/pages/category-summary/category-summary.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-summary',
+  standalone: true,
+  imports: [],
+  templateUrl: './category-summary.component.html',
+  styleUrl: './category-summary.component.scss'
+})
+export class CategorySummaryComponent {
+
+}
---
+++ src/app/modules/layout/pages/category-layout/category-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './category-layout.component.html',
+  styleUrl: './category-layout.component.scss'
+})
+export class CategoryLayoutComponent {
+
+}
