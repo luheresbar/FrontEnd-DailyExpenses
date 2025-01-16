bc8730310b0873b26a280b1e2c9ac058fad5b806
luheresbar <luheresbar@gmail.com>
2025-01-15 22:38:13 -0500

    Config https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
-  API_URL: 'https://backend-dailyexpenses-render.onrender.com'
+  // API_URL: 'https://backend-dailyexpenses-render.onrender.com'
+  // API_URL: 'http://18.220.253.156:8081'
+  API_URL: 'https://dailyexpenses.ddns.net'
+
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   // API_URL: 'http://localhost:8081'
-  API_URL: 'http://18.220.253.156:8081'
+  // API_URL: 'http://18.220.253.156:8081'
+  API_URL: 'https://dailyexpenses.ddns.net'
   // API_URL: 'https://backend-dailyexpenses-render.onrender.com'
 }
