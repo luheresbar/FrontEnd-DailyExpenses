dbcc0f0d5b89ab25a50deacc680aa1050eaf8e5c
luheresbar <luheresbar@gmail.com>
2025-01-15 19:43:44 -0500

    config backend aws

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  API_URL: 'http://localhost:8081'
+  // API_URL: 'http://localhost:8081'
+  API_URL: 'http://18.220.253.156:8081'
   // API_URL: 'https://backend-dailyexpenses-render.onrender.com'
 }
